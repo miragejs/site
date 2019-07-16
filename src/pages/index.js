0464af69f27206816047158e09818c81f776a1a3
Sam Selikoff <sam.selikoff@gmail.com>
2019-07-16 16:04:33 -0400

    wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,6 +39,28 @@ function Text({ children }) {
   )
 }
 
+function Spacer({ children, size = "md" }) {
+  let classes
+
+  switch (size) {
+    case "md":
+      classes = "mt-5 md:mt-7"
+      break
+
+    case "lg":
+      classes = "mt-8 lg:mt-12"
+      break
+
+    case "xl":
+      classes = "mt-12"
+      break
+
+    default:
+  }
+
+  return <div className={classes}>{children}</div>
+}
+
 function IndexPage() {
   let [activeTab, setActiveTab] = useState(0)
   let [refresh, setRefresh] = useState(0)
@@ -104,234 +126,230 @@ function IndexPage() {
 
         <section className="bg-white text-gray-5 py-16 md:py-20 lg:py-24">
           <Container>
-            <div>
-              <Title>
-                Have you ever worked on a React or Vue app that needed to talk
-                to a backend API before it was ready?
-              </Title>
+            <Title>
+              Have you ever worked on a React or Vue app that needed to talk to
+              a backend API before it was ready?
+            </Title>
+
+            <Spacer />
+
+            <Text>If so, how’d you handle it?</Text>
+
+            <Spacer />
+
+            <Text>
+              Maybe you created some local mock data directly in your app, just
+              to keep you moving:
+            </Text>
+
+            <Spacer size="lg" />
+
+            <div className="sm:rounded-lg overflow-hidden -mx-5">
+              <Snippet name="1-local-mock-data" />
             </div>
 
-            <div className="mt-5 md:mt-7">
-              <Text>If so, how’d you handle it?</Text>
+            <Spacer size="lg" />
+
+            <Text>Seems harmless enough.</Text>
+
+            <Spacer />
+
+            <Text>
+              Weeks later, the server’s ready and you wire up your app — but
+              nothing works like it did during development.
+            </Text>
+
+            <Spacer />
+
+            <Text>
+              Some screens flash with missing data, others are broken entirely,
+              and worst of all, you have no idea how much of your code needs to
+              be rewritten.
+            </Text>
+
+            <Spacer />
+
+            <Text>How did this happen?</Text>
+
+            <Spacer size="xl" />
+
+            <Title>You ignored the network for too long.</Title>
+
+            <Spacer />
+
+            <Text>
+              The network is the kind of thing that you can't just tack on to
+              the end of your sprint.
+            </Text>
+
+            <Spacer />
+
+            <Text>
+              Think about everything that goes into dealing with the network:
+              loading and error states, fetching partial data, caching — not to
+              mention the fact that asynchronous APIs like network requests add
+              a <em className="italic">ton</em> of new states to every one of
+              your app's existing user flows.
+            </Text>
+
+            <Spacer />
+
+            <Text>
+              By putting off the network, all these issues pile up and fall on
+              your lap after you've already written a ton of application code —
+              which is the hardest time to deal with them.
+            </Text>
+
+            <Spacer />
+
+            <Text>
+              The fact is that your local mock data setup poked one too many
+              holes in reality. Because of that, the code you wrote wasn't ready
+              for production.
+            </Text>
+
+            <Spacer size="xl" />
+
+            <Title>What if you could have the best of both worlds?</Title>
+
+            <Spacer />
+
+            <Text>
+              What if you could still mock data in the frontend, but also ensure
+              that your app only ever accessed that mock data the exact same way
+              it accesses real production data?
+            </Text>
+
+            <Spacer />
+
+            <Text>With Mirage.js, you can.</Text>
+
+            <Spacer />
+
+            <Text>Here's what it looks like:</Text>
+
+            <Spacer size="lg" />
+
+            <div className="flex text-center sm:bg-gray-900 sm:-mx-5 sm:border-b sm:border-gray-600 sm:px-5 sm:pt-1 sm:rounded-t-lg overflow-hidden">
+              <button
+                onClick={() => setActiveTab(0)}
+                className={`w-1/2 sm:w-auto sm:px-4 hover:text-gray-900 focus:outline-none border-b py-2  ${
+                  activeTab === 0
+                    ? "text-gray-900 border-gray-900"
+                    : "text-gray-400 border-gray-200 sm:border-transparent"
+                }`}
+              >
+                index.js
+              </button>
+              <button
+                onClick={() => setActiveTab(1)}
+                className={`w-1/2 sm:w-auto sm:px-4 hover:text-gray-900 focus:outline-none border-b py-2  ${
+                  activeTab === 1
+                    ? "text-gray-900 border-gray-900"
+                    : "text-gray-400 border-gray-200 sm:border-transparent"
+                }`}
+              >
+                App.js
+              </button>
             </div>
 
-            <div className="mt-5 md:mt-7">
-              <Text>
-                Maybe you created some local mock data directly in your app,
-                just to keep you moving:
-              </Text>
+            <div className="-mx-5 mt-6 sm:mt-0 sm:rounded-b-lg overflow-hidden">
+              {activeTab === 0 ? (
+                <Snippet name="2-mirage-index" />
+              ) : (
+                <Snippet name="2-mirage-app" />
+              )}
             </div>
 
-            <div className="mt-8 lg:mt-12">
-              <div className="sm:rounded-lg overflow-hidden -mx-5">
-                <Snippet name="1-local-mock-data" />
-              </div>
+            <Spacer size="lg" />
+
+            <Text>
+              You start by defining your API endpoints in Mirage. When you boot
+              up your frontend, Mirage kicks in automatically. There's no
+              separate server process for you to manage in yet-another terminal
+              tab.
+            </Text>
+
+            <Spacer />
+
+            <Text>
+              Now your components can fetch data from your API endpoints, just
+              as if there was a real server. Your components won't contain a
+              single line of code that reveals you're using Mirage
+              under-the-hood, because they won't even know they're talking to
+              Mirage instead of a real API.
+            </Text>
+
+            <Spacer />
+
+            <Text>
+              With Mirage, you'll never have to write throwaway network code
+              again.
+            </Text>
+
+            <Spacer size="xl" />
+
+            <Title>Mirage is a server that runs in the browser.</Title>
+
+            <Spacer />
+
+            <Text>
+              And it comes with all the power you'd expect from a real server.
+              You can tweak things like latency, error codes, and HTTP headers.
+              There's an in-memory database that lets you persist data. And you
+              can even write tests against all this functionality.
+            </Text>
+
+            <Spacer />
+
+            <Text>
+              With Mirage, you can build fully dynamic features entirely in your
+              frontend codebase, without having to wire up a production server.
+            </Text>
+
+            <Spacer size="xl" />
+
+            <Title>Live demo</Title>
+
+            <Spacer />
+
+            <Text>
+              Here's a complete Todo application, including server-side
+              persistence logic, built with React and Mirage:
+            </Text>
+
+            <Spacer size="xl" />
+
+            <div className="max-w-sm mx-auto">
+              <TodoApp refresh={refresh} />
             </div>
 
-            <div className="mt-8 lg:mt-12">
-              <Text>Seems harmless enough.</Text>
+            <div className="mt-4">
+              <button
+                onClick={resetApp}
+                className="text-sm text-blue-500 focus:outline-none px-3 py-2 mx-auto flex items-center hover:underline"
+              >
+                Reset App
+                <Replay className="ml-1 w-4 h-4" />
+              </button>
             </div>
 
-            <div className="mt-5 md:mt-7">
-              <Text>
-                Weeks later, the server’s ready and you wire up your app — but
-                nothing works like it did during development.
-              </Text>
-            </div>
+            <Spacer size="xl" />
 
-            <div className="mt-5 md:mt-7">
-              <Text>
-                Some screens flash with missing data, others are broken
-                entirely, and worst of all, you have no idea how much of your
-                code needs to be rewritten.
-              </Text>
-            </div>
-
-            <div className="mt-5 md:mt-7">
-              <Text>How did this happen?</Text>
-            </div>
-
-            <div className="mt-12">
-              <Title>You ignored the network for too long.</Title>
-            </div>
-
-            <div className="mt-5 md:mt-7">
-              <Text>
-                The network is the kind of thing that you can't just tack on to
-                the end of your sprint.
-              </Text>
-            </div>
-
-            <div className="mt-5 md:mt-7">
-              <Text>
-                Think about everything that goes into dealing with the network:
-                loading and error states, fetching partial data, caching — not
-                to mention the fact that asynchronous APIs like network requests
-                add a <em className="italic">ton</em> of new states to every one
-                of your app's existing user flows.
-              </Text>
-            </div>
-
-            <div className="mt-5 md:mt-7">
-              <Text>
-                By putting off the network, all these issues pile up and fall on
-                your lap after you've already written a ton of application code
-                — which is the hardest time to deal with them.
-              </Text>
-            </div>
-
-            <div className="mt-5 md:mt-7">
-              <Text>
-                The fact is that your local mock data setup poked one too many
-                holes in reality. Because of that, the code you wrote wasn't
-                ready for production.
-              </Text>
-            </div>
-
-            <div className="mt-12">
-              <Title>What if you could have the best of both worlds?</Title>
-            </div>
-
-            <div className="mt-5 md:mt-7">
-              <Text>
-                What if you could still mock data in the frontend, but also
-                ensure that your app only ever accessed that mock data the exact
-                same way it accesses real production data?
-              </Text>
-            </div>
-
-            <div className="mt-5 md:mt-7">
-              <Text>With Mirage.js, you can.</Text>
-            </div>
-
-            <div className="mt-5 md:mt-7">
-              <Text>Here's what it looks like:</Text>
-            </div>
-
-            <div className="mt-8 lg:mt-12">
-              <div className="flex text-center sm:bg-gray-900 sm:-mx-5 sm:border-b sm:border-gray-600 sm:px-5 sm:pt-1 sm:rounded-t-lg overflow-hidden">
-                <button
-                  onClick={() => setActiveTab(0)}
-                  className={`w-1/2 sm:w-auto sm:px-4 hover:text-gray-900 focus:outline-none border-b py-2  ${
-                    activeTab === 0
-                      ? "text-gray-900 border-gray-900"
-                      : "text-gray-400 border-gray-200 sm:border-transparent"
-                  }`}
-                >
-                  index.js
-                </button>
-                <button
-                  onClick={() => setActiveTab(1)}
-                  className={`w-1/2 sm:w-auto sm:px-4 hover:text-gray-900 focus:outline-none border-b py-2  ${
-                    activeTab === 1
-                      ? "text-gray-900 border-gray-900"
-                      : "text-gray-400 border-gray-200 sm:border-transparent"
-                  }`}
-                >
-                  App.js
-                </button>
-              </div>
-
-              <div className="-mx-5 mt-6 sm:mt-0 sm:rounded-b-lg overflow-hidden">
-                {activeTab === 0 ? (
-                  <Snippet name="2-mirage-index" />
-                ) : (
-                  <Snippet name="2-mirage-app" />
-                )}
-              </div>
-            </div>
-
-            <div className="mt-7">
-              <Text>
-                You start by defining your API endpoints in Mirage. When you
-                boot up your frontend, Mirage kicks in automatically. There's no
-                separate server process for you to manage in yet-another
-                terminal tab.
-              </Text>
-            </div>
-
-            <div className="mt-5 md:mt-7">
-              <Text>
-                Now your components can fetch data from your API endpoints, just
-                as if there was a real server. Your components won't contain a
-                single line of code that reveals you're using Mirage
-                under-the-hood, because they won't even know they're talking to
-                Mirage instead of a real API.
-              </Text>
-            </div>
-
-            <div className="mt-5 md:mt-7">
-              <Text>
-                With Mirage, you'll never have to write throwaway network code
-                again.
-              </Text>
-            </div>
-
-            <div className="mt-12">
-              <Title>Mirage is a server that runs in the browser.</Title>
-            </div>
-
-            <div className="mt-5 md:mt-7">
-              <Text>
-                And it comes with all the power you'd expect from a real server.
-                You can tweak things like latency, error codes, and HTTP
-                headers. There's an in-memory database that lets you persist
-                data. And you can even write tests against all this
-                functionality.
-              </Text>
-            </div>
-
-            <div className="mt-5 md:mt-7">
-              <Text>
-                With Mirage, you can build fully dynamic features entirely in
-                your frontend codebase, without having to wire up a production
-                server.
-              </Text>
-            </div>
-
-            <div className="mt-12">
-              <Title>Live demo</Title>
-            </div>
-
-            <div className="mt-5 md:mt-7">
-              <Text>
-                Here's a complete Todo application, including server-side
-                persistence logic, built with React and Mirage:
-              </Text>
-            </div>
-
-            <div className="mt-12 md:mt-20">
-              <div className="max-w-sm mx-auto">
-                <TodoApp refresh={refresh} />
-              </div>
-
-              <div className="mt-4">
-                <button
-                  onClick={resetApp}
-                  className="text-sm text-blue-500 focus:outline-none px-3 py-2 mx-auto flex items-center hover:underline"
-                >
-                  Reset App
-                  <Replay className="ml-1 w-4 h-4" />
-                </button>
-              </div>
-            </div>
-
-            <div className="mt-12 md:mt-20">
-              <Text>
-                <span className="xl:hidden">
-                  This app is running in the browser, but Mirage is intercepting
-                  all the React app's network requests.
-                </span>
-                <span className="hidden xl:visible">
-                  This app is running in the browser. If you take a look at the
-                  console, you'll see Mirage intercepting all of the React app's
-                  network requests.
-                </span>{" "}
-                The state is not actually being persisted — but the React app is
-                ready to be deployed to production, and talk to a real server
-                that fulfills the same API contract as Mirage.
-              </Text>
-            </div>
+            <Text>
+              <span className="xl:hidden">
+                This app is running in the browser, but Mirage is intercepting
+                all the React app's network requests.
+              </span>
+              <span className="hidden xl:visible">
+                This app is running in the browser. If you take a look at the
+                console, you'll see Mirage intercepting all of the React app's
+                network requests.
+              </span>{" "}
+              The state is not actually being persisted — but the React app is
+              ready to be deployed to production, and talk to a real server that
+              fulfills the same API contract as Mirage.
+            </Text>
           </Container>
         </section>
 

a12c33e0c4ed22eaf6fbc9b803b6a241c4dcfbb0
Ryan Toronto <ryanto@gmail.com>
2019-07-16 21:53:23 -0400

    make it work in node

--- src/lib/persist.js
+++ src/lib/persist.js
@@ -5,35 +5,44 @@ import server from "../server"
 // increase this number to bust all locally stored mirage
 // databases
 let currentVersion = 4
-let initialData = server._config.fixtures
-let originalHandled = server.pretender.handledRequest
 
-let saveDb = function() {
-  localStorage.setItem("mirage:db:version", currentVersion)
-  localStorage.setItem("mirage:db:data", JSON.stringify(server.db.dump()))
-}
+let resetDb
+let loadDb
 
-let resetDb = function(data = initialData) {
-  server.db.emptyData()
-  server.db.loadData(data)
-  saveDb()
-}
+if (server) {
+  let initialData = server._config.fixtures
+  let originalHandled = server.pretender.handledRequest
 
-let loadDb = function() {
-  let data
-  let version = localStorage.getItem("mirage:db:version")
-  let dataString = localStorage.getItem("mirage:db:data")
-
-  if (dataString && version === currentVersion.toString()) {
-    try {
-      resetDb(JSON.parse(dataString))
-    } catch (e) {}
+  let saveDb = function() {
+    localStorage.setItem("mirage:db:version", currentVersion)
+    localStorage.setItem("mirage:db:data", JSON.stringify(server.db.dump()))
   }
 
-  server.pretender.handledRequest = function() {
-    originalHandled.call(server.pretender, ...arguments)
+  resetDb = function(data = initialData) {
+    server.db.emptyData()
+    server.db.loadData(data)
     saveDb()
   }
+
+  loadDb = function() {
+    let data
+    let version = localStorage.getItem("mirage:db:version")
+    let dataString = localStorage.getItem("mirage:db:data")
+
+    if (dataString && version === currentVersion.toString()) {
+      try {
+        resetDb(JSON.parse(dataString))
+      } catch (e) {}
+    }
+
+    server.pretender.handledRequest = function() {
+      originalHandled.call(server.pretender, ...arguments)
+      saveDb()
+    }
+  }
+} else {
+  resetDb = () => {}
+  loadDb = () => {}
 }
 
 export { loadDb, resetDb }

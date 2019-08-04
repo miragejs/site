0a440f82e6d20b04c0c9ed94fec4d2af4a88c8e2
Sam Selikoff <sam.selikoff@gmail.com>
2019-08-04 02:19:31 -0400

    esdoc stuff - wip for now

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,6 +5,8 @@
  */
 const fs = require("fs")
 
+// I think this was from an earlier strategy at getting the snippet contents, but
+// we went back to remark because it supports line highlighting
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
 
@@ -33,3 +35,52 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
     })
   }
 }
+
+// const esdoc = require("esdoc").default
+// const ESDOC_CONFIG = {
+//   source: "./node_modules/@miragejs/server/lib",
+//   destination: "./",
+//   excludes: ["(node_modules|tests|tmp)"],
+//   plugins: [
+//     {
+//       name: "esdoc-ecmascript-proposal-plugin",
+//       option: {
+//         classProperties: true,
+//         objectRestSpread: true,
+//         doExpressions: true,
+//         functionBind: true,
+//         functionSent: true,
+//         asyncGenerators: true,
+//         decorators: true,
+//         exportExtensions: true,
+//         dynamicImport: true,
+//       },
+//     },
+//     { name: "esdoc-accessor-plugin" },
+//   ],
+// }
+// exports.createPages = async ({ actions: { createPage } }) => {
+//   esdoc.generate(ESDOC_CONFIG)
+//   console.log(fs)
+//   console.log(esdoc)
+//   // debugger
+//
+//   // Example from Gatsby docs
+//   // `getPokemonData` is a function that fetches our data
+//   // const allPokemon = await getPokemonData(["pikachu", "charizard", "squirtle"])
+//   // Create a page that lists all Pokémon.
+//   // createPage({
+//   //   path: `/`,
+//   //   component: require.resolve("./src/templates/all-pokemon.js"),
+//   //   context: { allPokemon },
+//   // })
+//   //
+//   // // Create a page for each Pokémon.
+//   // allPokemon.forEach(pokemon => {
+//   //   createPage({
+//   //     path: `/pokemon/${pokemon.name}/`,
+//   //     component: require.resolve("./src/templates/pokemon.js"),
+//   //     context: { pokemon },
+//   //   })
+//   // })
+// }

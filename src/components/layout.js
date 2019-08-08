026df9c9ea27b4cb2fcc8643aa672aac33706e85
Sam Selikoff <sam.selikoff@gmail.com>
2019-08-08 09:12:58 -0400

    Mobile nav

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react"
+import React, { useState, useContext } from "react"
 import Logo from "../assets/images/logo.svg"
 import { Link } from "gatsby"
 import { useStaticQuery, graphql } from "gatsby"
@@ -18,6 +18,37 @@ const themeClasses = {
   },
 }
 
+function MobileNavLink(props) {
+  const theme = useContext(ThemeContext)
+  const baseClasses = `block py-2 font-medium`
+  const isExternal = props.to.indexOf("http") === 0
+  let link
+
+  if (isExternal) {
+    link = (
+      <a
+        href={props.to}
+        {...props}
+        className={`${baseClasses} ${themeClasses[theme]["inactive"]}`}
+      >
+        {props.children}
+      </a>
+    )
+  } else {
+    const isPartiallyActive = ({ isPartiallyCurrent }) => {
+      let state = isPartiallyCurrent ? "active" : "inactive"
+
+      return {
+        className: `${baseClasses} ${themeClasses[theme][state]}`,
+      }
+    }
+
+    link = <Link {...props} getProps={isPartiallyActive} />
+  }
+
+  return <div className="border-b border-gray-800 py-1">{link}</div>
+}
+
 function NavLink(props) {
   const theme = useContext(ThemeContext)
   const isPartiallyActive = ({ isPartiallyCurrent }) => {
@@ -34,6 +65,8 @@ function NavLink(props) {
 const ThemeContext = React.createContext({ variant: "light" })
 
 export default function(props) {
+  let [isShowingMobileNav, setIsShowingMobileNav] = useState(false)
+
   const data = useStaticQuery(graphql`
     query AllSitePageQuery {
       allSitePage {
@@ -70,37 +103,90 @@ export default function(props) {
   return (
     <ThemeContext.Provider value={theme}>
       <div className="antialiased text-gray-700 font-body font-light leading-normal min-h-screen flex flex-col">
-        <div className={`relative ${theme === "light" && "bg-white shadow"}`}>
-          <div className="mx-auto max-w-lg md:max-w-3xl lg:max-w-4xl xl:max-w-6xl 2xl:max-w-8xl px-5 md:px-8">
-            <header className="py-2 flex items-center">
-              <Link to="/">
-                <Logo
-                  className={`w-8 h-8 md:w-10 md:h-16 ${
-                    theme === "dark" ? "text-green-500" : "text-gray-900"
-                  }`}
-                />
-              </Link>
+        <div
+          className={`relative z-10 ${
+            theme === "light" ? "bg-white shadow" : ""
+          }`}
+        >
+          <div className="mx-auto max-w-lg md:max-w-3xl lg:max-w-4xl xl:max-w-6xl 2xl:max-w-8xl md:px-8">
+            <header className="pt-2 px-1">
+              <div className="flex items-center">
+                <Link to="/" className="px-4 py-2">
+                  <Logo
+                    className={`w-8 h-8 md:w-10 md:h-16 ${
+                      theme === "dark" ? "text-green-500" : "text-gray-900"
+                    }`}
+                  />
+                </Link>
 
-              <NavLink to="/docs">Documentation</NavLink>
-
-              <div className="ml-auto">
-                <a
-                  href="https://github.com/miragejs/server"
-                  className={themeClasses[theme]["inactive"]}
-                >
-                  <svg
-                    className="fill-current h-7"
-                    viewBox="0 0 16 16"
-                    version="1.1"
-                    aria-hidden="true"
+                {/* Mobile nav button */}
+                <div className="ml-auto md:hidden">
+                  <button
+                    onClick={() => setIsShowingMobileNav(!isShowingMobileNav)}
+                    className="flex px-4 py-3 items-center lg:hidden text-gray-500 focus:outline-none focus:text-gray-300"
                   >
-                    <path
-                      fillRule="evenodd"
-                      d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"
-                    ></path>
-                  </svg>
-                </a>
+                    {isShowingMobileNav ? (
+                      <svg
+                        className="fill-current w-4 h-4"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                      >
+                        <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"></path>
+                      </svg>
+                    ) : (
+                      <svg
+                        className="fill-current w-4 h-4"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                      >
+                        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+                      </svg>
+                    )}
+                  </button>
+                </div>
+
+                {/* Desktop nav */}
+                <div className="hidden md:flex md:w-full">
+                  <NavLink to="/docs">Documentation</NavLink>
+
+                  <div className="ml-auto">
+                    <a
+                      href="https://github.com/miragejs/server"
+                      className={themeClasses[theme]["inactive"]}
+                    >
+                      <svg
+                        className="fill-current h-7"
+                        viewBox="0 0 16 16"
+                        version="1.1"
+                        aria-hidden="true"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"
+                        ></path>
+                      </svg>
+                    </a>
+                  </div>
+                </div>
               </div>
+
+              {/* Mobile nav */}
+              {isShowingMobileNav && (
+                <nav className="mx-4">
+                  <MobileNavLink
+                    to="/docs"
+                    onClick={() => setIsShowingMobileNav(false)}
+                  >
+                    Documentation
+                  </MobileNavLink>
+                  <MobileNavLink
+                    to="https://github.com/miragejs/server"
+                    onClick={() => setIsShowingMobileNav(false)}
+                  >
+                    GitHub
+                  </MobileNavLink>
+                </nav>
+              )}
             </header>
           </div>
         </div>

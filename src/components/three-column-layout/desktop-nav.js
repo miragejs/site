f0c839baa2406485b4467244068682c480b88b8b
Sam Selikoff <sam.selikoff@gmail.com>
2019-08-13 19:14:05 -0400

    extract ThreeColumnLayout

---
+++ src/components/three-column-layout/desktop-nav.js
@@ -0,0 +1,101 @@
+import React from "react"
+import { Link } from "@reach/router"
+
+const MAX_WIDTH = 1400
+const MAIN_WIDTH = 870
+const SIDEBAR_WIDTH = (MAX_WIDTH - MAIN_WIDTH) / 2
+
+export function DesktopLeftNav(props) {
+  return (
+    <div
+      className="flex-shrink-0 bg-gray-100 border-r border-gray-200 hidden lg:block min-w-56"
+      style={{
+        width: `calc(((100% - ${MAX_WIDTH}px)/ 2) + ${SIDEBAR_WIDTH}px)`,
+        paddingLeft: `calc((100% - ${MAX_WIDTH}px)/ 2)`,
+      }}
+    >
+      <nav className="px-8 pt-8 xl:pt-14 sticky top-0 leading-none h-screen overflow-y-scroll">
+        <ul className="mt-2">
+          {props.routes.map(route => (
+            <li className="mb-8" key={route.fullPath}>
+              <span className="text-gray-800 text-base+ font-medium">
+                {route.label}
+              </span>
+              <ul className="ml-2 mt-2 font-normal leading-snug">
+                {route.routes.map(route => (
+                  <DesktopLeftNavLink
+                    fullPath={route.fullPath}
+                    key={route.fullPath}
+                  >
+                    {route.label}
+                  </DesktopLeftNavLink>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+function DesktopLeftNavLink({ fullPath, ...props }) {
+  const isPartiallyActive = ({ isPartiallyCurrent }) => {
+    return {
+      className: isPartiallyCurrent
+        ? "text-gray-900"
+        : "text-gray-600 hover:text-gray-900",
+    }
+  }
+
+  return (
+    <li className="py-1" key={props.route}>
+      <Link getProps={isPartiallyActive} to={fullPath} {...props} />
+    </li>
+  )
+}
+
+export function DesktopRightNav(props) {
+  return (
+    <div
+      className="hidden xl:block flex-shrink-0"
+      style={{
+        width: `calc(((100% - ${MAX_WIDTH}px)/ 2) + ${SIDEBAR_WIDTH}px)`,
+        paddingRight: `calc((100% - 1408px)/ 2)`,
+      }}
+    >
+      <div className="pr-8">
+        <nav className="mt-32 ml-8 sticky">
+          {props.currentPageTableOfContentsItems && (
+            <>
+              <p className="uppercase text-xs text-gray-800 font-medium tracking-wider">
+                On this page
+              </p>
+
+              <ul className="mt-2 font-normal text-sm">
+                {props.currentPageTableOfContentsItems.map(item => (
+                  <li key={item.url} className="my-2 font-medium text-blue-500">
+                    {item.title}
+
+                    {item.items && (
+                      <ul className="pl-4">
+                        {item.items.map(item => (
+                          <li
+                            key={item.url}
+                            className="my-2 font-medium text-blue-500"
+                          >
+                            {item.title}
+                          </li>
+                        ))}
+                      </ul>
+                    )}
+                  </li>
+                ))}
+              </ul>
+            </>
+          )}
+        </nav>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/three-column-layout/index.js
@@ -0,0 +1,143 @@
+import React from "react"
+import { DesktopLeftNav, DesktopRightNav } from "./desktop-nav"
+import { MobileNav } from "./mobile-nav"
+import { MDXProvider } from "@mdx-js/react"
+import Code from "../code"
+
+export function ThreeColumnLayout(props) {
+  let currentPageTableOfContentsItems =
+    props.currentPageTableOfContentsItems || []
+
+  return (
+    <div className="bg-white">
+      <MobileNav routes={props.routes} />
+
+      <div className="flex-1 flex">
+        <DesktopLeftNav
+          routes={props.routes}
+          currentPageTableOfContentsItems={currentPageTableOfContentsItems}
+        />
+
+        <div
+          className="flex-1 w-full px-5 pt-7 font-normal text-gray-700
+            text-base leading-copy
+            sm:pt-8
+            md:leading-relaxed md:pt-8 md:px-20
+            lg:pt-12
+          "
+        >
+          <div
+            className="max-w-md mx-auto
+            md:max-w-2xl md:text-lg
+            lg:ml-0
+            xl:mx-auto
+          "
+          >
+            <MDXProvider components={components}>{props.children}</MDXProvider>
+          </div>
+        </div>
+
+        <DesktopRightNav
+          currentPageTableOfContentsItems={currentPageTableOfContentsItems}
+        />
+      </div>
+    </div>
+  )
+}
+
+const components = {
+  h1: props => (
+    <h1
+      {...props}
+      className="text-gray-900 font-title font-normal leading-tight
+        mb-6 text-4xl
+        md:mb-8 md:text-5xl
+      "
+    >
+      {props.children}
+    </h1>
+  ),
+
+  h2: props => (
+    <h2
+      {...props}
+      className="text-gray-900 font-normal font-title
+        text-2xl leading-tight mt-10 mb-4
+        md:text-3xl md:mt-14 md:mb-6
+      "
+    >
+      {props.children}
+    </h2>
+  ),
+
+  h3: props => (
+    <h3
+      {...props}
+      className="text-gray-900 font-normal font-title
+        mt-12
+        mb-5 md:mb-6
+        text-2-25xl leading-normal
+      "
+    >
+      {props.children}
+    </h3>
+  ),
+
+  p: props => (
+    <p {...props} className="my-5">
+      {props.children}
+    </p>
+  ),
+
+  ol: props => (
+    <ol {...props} className="ml-8 list-decimal">
+      {props.children}
+    </ol>
+  ),
+
+  ul: props => (
+    <ul {...props} className="ml-8 list-disc">
+      {props.children}
+    </ul>
+  ),
+
+  li: props => (
+    <li {...props} className="md:pl-2">
+      {props.children}
+    </li>
+  ),
+
+  strong: props => (
+    <strong {...props} className="font-medium">
+      {props.children}
+    </strong>
+  ),
+
+  a: props => (
+    <a {...props} className="underline text-blue-500">
+      {props.children}
+    </a>
+  ),
+
+  hr: props => (
+    <hr {...props} className="border-t border-gray-300 mt-6 mb-8 md:my-8" />
+  ),
+
+  inlineCode: props => (
+    <code
+      {...props}
+      className="font-mono text-base px-1"
+      style={{ backgroundColor: "rgba(255, 229, 100, 0.2)" }}
+    >
+      {props.children}
+    </code>
+  ),
+
+  pre: props => <div {...props} />,
+
+  code: props => (
+    <div className="sm:rounded-lg overflow-hidden -mx-5 md:mx-0 my-8 md:my-10">
+      <Code {...props} />
+    </div>
+  ),
+}

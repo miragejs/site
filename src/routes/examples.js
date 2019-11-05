99f465a90fa922747a608e9360dcaf25881b9c20
Ryan T <ryanto@gmail.com>
2019-11-05 13:39:26 -0500

    Getting started with React (#65)
    * Getting started with React and testing
    * Update React example and add table of contents
    
    Co-authored-by: @nadnoslen

--- src/routes/examples.js
+++ src/routes/examples.js
@@ -1,17 +1,44 @@
+import { graphql, useStaticQuery } from "gatsby"
 import React from "react"
 import { ThreeColumnLayout } from "../components/three-column-layout"
 import { useRouter } from "../hooks/use-router"
 
-export default function DocsPage(props) {
+export default function ExamplesPage(props) {
   let router = useRouter()
 
   let docsRouter = router.routerFor("/examples")
+
+  let data = useStaticQuery(graphql`
+    query OnThisExamplePage {
+      allMdx {
+        nodes {
+          tableOfContents
+          fileAbsolutePath
+        }
+      }
+    }
+  `)
+
+  let mdxPage = data.allMdx.nodes.find(node => {
+    let didMatch = false
+    let match = node.fileAbsolutePath.match(/(\/examples\/.+)\.md[x]?/)
+
+    if (match) {
+      let regexp = new RegExp(`${match[1]}/?`)
+      didMatch = match && regexp.test(props.location.pathname)
+    }
+
+    return didMatch
+  })
+
+  let tableOfContentsItems = mdxPage && mdxPage.tableOfContents.items[0].items
 
   return (
     <ThreeColumnLayout
       routes={docsRouter.routes}
       previousPage={docsRouter.previousPage}
       nextPage={docsRouter.nextPage}
+      currentPageTableOfContentsItems={tableOfContentsItems}
     >
       {props.children}
     </ThreeColumnLayout>

70782ce521fd1bb28e11b1dced2c2f15f456ae5f
Sam Selikoff <sam.selikoff@gmail.com>
2020-01-15 16:23:19 -0500

    tweak layout

--- src/routes/app.js
+++ src/routes/app.js
@@ -73,9 +73,9 @@ function AppInner(props) {
       <div className="flex flex-col min-h-screen antialiased leading-normal text-gray-700 font-body">
         <Header showHeaderNav={showHeaderNav} />
 
-        {/* <main className="flex flex-col flex-1">
+        <main className="flex flex-col flex-1">
           <Outlet />
-        </main> */}
+        </main>
 
         <Footer />
       </div>
@@ -94,150 +94,152 @@ function Header({ showHeaderNav }) {
         theme === "dark" ? "bg-gray-1000" : "bg-white shadow"
       }`}
     >
-      <div className="mx-auto max-w-8xl md:px-8">
-        <header
-          className={`lg:py-2
-            ${isShowingMobileNav && theme === "dark" ? "bg-gray-900" : ""}
-          `}
-        >
-          <div className="flex items-center">
-            <Link
-              to="/"
-              className={`px-5 py-2 md:px-0 flex items-center flex-shrink-0  ${
-                // theme === "dark" ? "text-green-500" : "text-gray-900"
-                theme === "dark" ? "text-green-500" : "text-green-500"
-              }`}
-              onClick={() => setIsShowingMobileNav(false)}
-            >
-              <LogoAndName
-                className={`
-                ${
+      <div className="md:px-8">
+        <div className="mx-auto max-w-7xl">
+          <header
+            className={`lg:py-2
+              ${isShowingMobileNav && theme === "dark" ? "bg-gray-900" : ""}
+            `}
+          >
+            <div className="flex items-center">
+              <Link
+                to="/"
+                className={`px-5 py-2 md:px-0 flex items-center flex-shrink-0  ${
+                  // theme === "dark" ? "text-green-500" : "text-gray-900"
+                  theme === "dark" ? "text-green-500" : "text-green-500"
+                }`}
+                onClick={() => setIsShowingMobileNav(false)}
+              >
+                <LogoAndName
+                  className={`
+                  ${
+                    theme === "dark"
+                      ? "w-24 md:w-28 lg:w-34 py-3 text-gray-100"
+                      : "w-24 md:w-28 py-2 lg:py-1 text-gray-900"
+                  }
+                `}
+                />
+              </Link>
+
+              {/* Leaving out for now, but want to make this work. Just need to figure out a treatment for homepage. */}
+              {/* <span
+                style={{ marginLeft: "125px", marginRight: "50px" }}
+                className={`hidden lg:block py-4 border-l ${
                   theme === "dark"
-                    ? "w-24 md:w-28 lg:w-34 py-3 text-gray-100"
-                    : "w-24 md:w-28 py-2 lg:py-1 text-gray-900"
-                }
-              `}
-              />
-            </Link>
+                    ? "border-gray-700"
+                    : "lg:block border-gray-200"
+                }`}
+              ></span> */}
 
-            {/* Leaving out for now, but want to make this work. Just need to figure out a treatment for homepage. */}
-            {/* <span
-              style={{ marginLeft: "125px", marginRight: "50px" }}
-              className={`hidden lg:block py-4 border-l ${
-                theme === "dark"
-                  ? "border-gray-700"
-                  : "lg:block border-gray-200"
-              }`}
-            ></span> */}
-
-            {/* Mobile nav button */}
-            {showHeaderNav ? (
-              <div className="ml-auto md:hidden">
-                <button
-                  onClick={() => setIsShowingMobileNav(!isShowingMobileNav)}
-                  className={`flex px-5 py-3 2xl:py-2 items-center focus:outline-none ${themeClasses[theme]["inactive"]} lg:hidden `}
-                >
-                  {isShowingMobileNav ? (
-                    <Close className="w-4 h-4" />
-                  ) : (
-                    <Menu className="w-4 h-4" />
-                  )}
-                </button>
-              </div>
-            ) : null}
-
-            {/* Desktop nav */}
-            <div
-              className={`hidden md:flex md:items-center md:ml-8 lg:ml-16
-                ${theme === "dark" ? "pt-1" : ""}`}
-            >
+              {/* Mobile nav button */}
               {showHeaderNav ? (
-                <>
-                  <NavLink
-                    to={router.routerFor("/docs").pages[0].fullPath}
-                    activeFor="/docs/*"
+                <div className="ml-auto md:hidden">
+                  <button
+                    onClick={() => setIsShowingMobileNav(!isShowingMobileNav)}
+                    className={`flex px-5 py-3 2xl:py-2 items-center focus:outline-none ${themeClasses[theme]["inactive"]} lg:hidden `}
+                  >
+                    {isShowingMobileNav ? (
+                      <Close className="w-4 h-4" />
+                    ) : (
+                      <Menu className="w-4 h-4" />
+                    )}
+                  </button>
+                </div>
+              ) : null}
+
+              {/* Desktop nav */}
+              <div
+                className={`hidden md:flex md:items-center md:ml-8 lg:ml-16
+                  ${theme === "dark" ? "pt-1" : ""}`}
+              >
+                {showHeaderNav ? (
+                  <>
+                    <NavLink
+                      to={router.routerFor("/docs").pages[0].fullPath}
+                      activeFor="/docs/*"
+                    >
+                      Guides
+                    </NavLink>
+                    <NavLink to="/api/classes/association" activeFor="/api/*">
+                      API
+                    </NavLink>
+                    <NavLink
+                      to={router.routerFor("/quickstarts").pages[0].fullPath}
+                      activeFor="/quickstarts/*"
+                    >
+                      Quickstarts
+                    </NavLink>
+                  </>
+                ) : null}
+              </div>
+
+              <div className="hidden md:flex md:items-center md:ml-auto">
+                <a
+                  href="https://discord.gg/pPsdsrn"
+                  className={`px-1 mr-5 ${themeClasses[theme]["inactive"]}`}
+                >
+                  <Discord className="h-5 fill-current" />
+                </a>
+                <a
+                  href="https://twitter.com/miragejs"
+                  className={`px-1 mr-5 ${themeClasses[theme]["inactive"]}`}
+                >
+                  <Twitter className="h-5 fill-current" />
+                </a>
+                <a
+                  href="https://github.com/miragejs/server"
+                  className={`px-1 ${themeClasses[theme]["inactive"]}`}
+                >
+                  <Github className="h-5 fill-current" />
+                </a>
+              </div>
+            </div>
+
+            {/* Mobile nav */}
+            {isShowingMobileNav && (
+              <nav className="text-lg font-medium md:hidden">
+                <div
+                  className={`border-t ${
+                    theme === "dark" ? "border-gray-800" : "border-gray-200"
+                  }`}
+                >
+                  <MobileNavLink
+                    to="/docs/getting-started/introduction"
+                    onClick={() => setIsShowingMobileNav(false)}
                   >
                     Guides
-                  </NavLink>
-                  <NavLink to="/api/classes/association" activeFor="/api/*">
-                    API
-                  </NavLink>
-                  <NavLink
-                    to={router.routerFor("/quickstarts").pages[0].fullPath}
-                    activeFor="/quickstarts/*"
+                  </MobileNavLink>
+                </div>
+
+                <div
+                  className={`border-t ${
+                    theme === "dark" ? "border-gray-800" : "border-gray-200"
+                  }`}
+                >
+                  <MobileNavLink
+                    to="/api/classes/association"
+                    onClick={() => setIsShowingMobileNav(false)}
                   >
-                    Quickstarts
-                  </NavLink>
-                </>
-              ) : null}
-            </div>
+                    API
+                  </MobileNavLink>
+                </div>
 
-            <div className="hidden md:flex md:items-center md:ml-auto">
-              <a
-                href="https://discord.gg/pPsdsrn"
-                className={`px-1 mr-5 ${themeClasses[theme]["inactive"]}`}
-              >
-                <Discord className="h-5 fill-current" />
-              </a>
-              <a
-                href="https://twitter.com/miragejs"
-                className={`px-1 mr-5 ${themeClasses[theme]["inactive"]}`}
-              >
-                <Twitter className="h-5 fill-current" />
-              </a>
-              <a
-                href="https://github.com/miragejs/server"
-                className={`px-1 ${themeClasses[theme]["inactive"]}`}
-              >
-                <Github className="h-5 fill-current" />
-              </a>
-            </div>
-          </div>
-
-          {/* Mobile nav */}
-          {isShowingMobileNav && (
-            <nav className="text-lg font-medium md:hidden">
-              <div
-                className={`border-t ${
-                  theme === "dark" ? "border-gray-800" : "border-gray-200"
-                }`}
-              >
-                <MobileNavLink
-                  to="/docs/getting-started/introduction"
-                  onClick={() => setIsShowingMobileNav(false)}
+                <div
+                  className={`border-t ${
+                    theme === "dark" ? "border-gray-800" : "border-gray-200"
+                  }`}
                 >
-                  Guides
-                </MobileNavLink>
-              </div>
-
-              <div
-                className={`border-t ${
-                  theme === "dark" ? "border-gray-800" : "border-gray-200"
-                }`}
-              >
-                <MobileNavLink
-                  to="/api/classes/association"
-                  onClick={() => setIsShowingMobileNav(false)}
-                >
-                  API
-                </MobileNavLink>
-              </div>
-
-              <div
-                className={`border-t ${
-                  theme === "dark" ? "border-gray-800" : "border-gray-200"
-                }`}
-              >
-                <MobileNavLink
-                  to="https://github.com/miragejs/server"
-                  onClick={() => setIsShowingMobileNav(false)}
-                >
-                  GitHub
-                </MobileNavLink>
-              </div>
-            </nav>
-          )}
-        </header>
+                  <MobileNavLink
+                    to="https://github.com/miragejs/server"
+                    onClick={() => setIsShowingMobileNav(false)}
+                  >
+                    GitHub
+                  </MobileNavLink>
+                </div>
+              </nav>
+            )}
+          </header>
+        </div>
       </div>
     </div>
   )

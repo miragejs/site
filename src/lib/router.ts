010054bc7ee0a3c2e62e5546fe9b4402325a91c9
Ryan Toronto <ryanto@gmail.com>
2020-03-02 18:29:09 -0500

    React Native touch-ups

--- src/lib/router.ts
+++ src/lib/router.ts
@@ -139,18 +139,19 @@ const allRoutes: RouteDefinition[] = [
           { label: "Production builds", name: "exclude-from-production" },
         ],
       },
-      { label: "Ember", name: "ember" },
-      { label: "Cypress", name: "cypress" },
       {
         label: "React Native",
         name: "react-native",
         routes: [
+          { label: "Development", name: "development" },
           {
             label: "React Native Testing Library",
             name: "react-native-testing-library",
           },
         ],
       },
+      { label: "Ember", name: "ember" },
+      { label: "Cypress", name: "cypress" },
     ],
   },
 ]

c6622cd4c9a1c125c94bf7f9944fa71d23d5e172
Sam Selikoff <sam.selikoff@gmail.com>
2020-06-29 20:41:15 -0400

    need a role ><

--- functions/auth.js
+++ functions/auth.js
@@ -1,6 +1,8 @@
 exports.handler = function (event, context, callback) {
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({}),
+    body: JSON.stringify({
+      "X-Hasura-Role": "user",
+    }),
   })
 }

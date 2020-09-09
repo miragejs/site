a1b7d8be895ad7ade863f4bf0f6d672966ad5e5d
Sam Selikoff <sam.selikoff@gmail.com>
2020-09-09 12:29:59 -0400

    tweak carbon css

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,15 +3,3 @@
  *
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
-// import React from "react"
-
-// export const onRenderBody = ({ setHeadComponents }) => {
-//   setHeadComponents([
-//     <script
-//       id="_carbonads_js"
-//       src="//cdn.carbonads.com/carbon.js?serve=CE7D42QY&placement=miragejscom"
-//       type="text/javascript"
-//       async
-//     />,
-//   ])
-// }
